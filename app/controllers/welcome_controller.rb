5533bdc99c64a611c905b8220cf14552163594cf
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2011-05-28 11:30:22 +0800

    init project

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+  def index
+    sc2ranks = Sc2ranks.new('localhost:3000')
+    debugger
+    character = sc2ranks.character('http://us.battle.net/sc2/en/profile/280904/1/Kelsin/')
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRumble::Application.config.secret_token = 'f8a000c9d097a903b12c594dab85fb6cbdb74c2ab8d345c4e8961d41439b9b3a7d2a0be1bb37487ae6a7c88fea8524b9fc37abac5589cfac85a90329b0809688'
