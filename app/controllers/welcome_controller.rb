8ce7d5805cc341523466f7f7a6228a026e980e44
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2011-05-28 12:47:39 +0800

    i dont always test my code. but when i do, it's on production

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @sc2ranks = Sc2ranks.new('malayancolleges.com')
-    @character = sc2ranks.character('http://us.battle.net/sc2/en/profile/280904/1/Kelsin/')
+    @character = @sc2ranks.character('http://us.battle.net/sc2/en/profile/280904/1/Kelsin/')
   end
 end
